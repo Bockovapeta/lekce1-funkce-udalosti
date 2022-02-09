e41a3db57b7f7b8a2f69353877b290a48c692176
Bockovapeta <91452657+Bockovapeta@users.noreply.github.com>
2022-02-09 19:00:36 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+function obarviNaZeleno() {
+  let ctverecek = document.querySelector('.ctverecek');
+  ctverecek.style.backgroundColor = "green";
+}
+
+
+function zobrazVysledek() {
+  document.querySelector("#vysledek").innerHTML = secti(4, 5);
+
+}
+
+
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -17,5 +28,12 @@ function secti(a, b) {
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
+  document.querySelector('.ctverecek').textContent = 'Gratulace';
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
   alert("Gratulace, právě jsi spustila tuto funkci!");
+
 }
+
+
+
+
